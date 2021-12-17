73a03852ca046d31f428b42137e30975cefa551c
kyliu <liukaiyuan@deltaphone.com.cn>
2021-12-17 10:35:07 +0800

    初始化简易提交

---
+++ js/index.js
@@ -0,0 +1,6 @@
+!(function () {
+    const div = document.createElement("div");
+    div.style.cssText =
+      "height:300px;width:300px;background:red;position:fixed;top:0;left:0;z-index:100";
+    // document.body.appendChild(div);
+})();
