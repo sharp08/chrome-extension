0ff9efe4231d99f3633d38c698db79d159b20db9
kyliu <liukaiyuan@deltaphone.com.cn>
2021-12-30 17:48:25 +0800

    添加图标及popup.html

--- js/index.js
+++ js/index.js
@@ -1,6 +1,10 @@
 !(function () {
-    const div = document.createElement("div");
-    div.style.cssText =
-      "height:300px;width:300px;background:red;position:fixed;top:0;left:0;z-index:100";
-    // document.body.appendChild(div);
+  const div = document.createElement("div");
+  div.style.cssText =
+    "height:300px;width:300px;background:red;position:fixed;top:0;left:0;z-index:99999";
+
+  // setInterval(() => {
+  //   div.innerHTML = `<span>${new Date()}</span>`;
+  // }, 1000);
+  // document.body.appendChild(div);
 })();
