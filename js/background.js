c6900db079cb8b205c29816b1f01542ab630a6f5
lky <liukaiyuan@deltaphone.com.cn>
2023-02-02 12:39:54 +0800

    'B站视频调速'

--- js/background.js
+++ js/background.js
@@ -1,6 +1,27 @@
-let color = "#3aa757";
-
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  console.log("Default background color set to %cgreen", `color: ${color}`);
+  chrome.action.setBadgeText({
+    text: "OFF"
+  });
+});
+
+chrome.action.onClicked.addListener(async tab => {
+  const prevState = await chrome.action.getBadgeText({
+    tabId: tab.id
+  });
+  // const nextState = prevState === "ON" ? "OFF" : "ON";
+  const nextState = "ON";
+
+  await chrome.action.setBadgeText({
+    tabId: tab.id,
+    text: nextState
+  });
+
+  if (nextState === "ON") {
+    await chrome.scripting.executeScript({
+      target: {
+        tabId: tab.id
+      },
+      files: ["./js/content-script.js"]
+    });
+  }
 });
---
+++ js/content-script.js
@@ -0,0 +1,14 @@
+!(function () {
+  const list = [3, 2.75, 2.5, 2.25, 2, 1.75, 1.5, 1.25, 1, 0.75, 0.5, 0.25];
+
+  let str = "";
+  for (let i = 0; i < list.length; i++) {
+    const v = list[i];
+    const _str = `<li class="bpx-player-ctrl-playbackrate-menu-item " data-value="${v}">${v}x</li>`;
+    str += _str;
+  }
+
+  const menuDom = document.querySelector(".bpx-player-ctrl-playbackrate-menu");
+
+  menuDom.innerHTML = str;
+})();
