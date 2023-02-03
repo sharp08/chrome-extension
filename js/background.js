00af9f59d7565db5bf03357db8b147fc512527f6
lky <liukaiyuan@deltaphone.com.cn>
2023-02-03 13:57:36 +0800

    改为自动加载

--- js/background.js
+++ js/background.js
@@ -8,20 +8,20 @@ chrome.action.onClicked.addListener(async tab => {
   const prevState = await chrome.action.getBadgeText({
     tabId: tab.id
   });
-  // const nextState = prevState === "ON" ? "OFF" : "ON";
-  const nextState = "";
+  const nextState = prevState === "ON" ? "OFF" : "ON";
 
   await chrome.action.setBadgeText({
     tabId: tab.id,
     text: nextState
   });
 
-  if (nextState === "") {
-    await chrome.scripting.executeScript({
-      target: {
-        tabId: tab.id
-      },
-      files: ["./js/content-script.js"]
-    });
+  // 切着玩，没什么鸟用
+  if (nextState === "ON") {
+    // await chrome.scripting.executeScript({
+    //   target: {
+    //     tabId: tab.id
+    //   },
+    //   files: ["./js/content-script.js"]
+    // });
   }
 });
--- js/content-script.js
+++ js/content-script.js
@@ -1,16 +1,31 @@
 !(function () {
-  const list = [3, 2.75, 2.5, 2.25, 2, 1.75, 1.5, 1.25, 1, 0.75, 0.5, 0.25];
+  function exec() {
+    const list = [3, 2.75, 2.5, 2.25, 2, 1.75, 1.5, 1.25, 1, 0.75, 0.5, 0.25];
 
-  let str = "";
-  for (let i = 0; i < list.length; i++) {
-    const v = list[i];
-    const _str = `<li class="bpx-player-ctrl-playbackrate-menu-item " data-value="${v}">${v}x</li>`;
-    str += _str;
+    let str = "";
+    for (let i = 0; i < list.length; i++) {
+      const v = list[i];
+      const _str = `<li class="bpx-player-ctrl-playbackrate-menu-item " data-value="${v}">${v}x</li>`;
+      str += _str;
+    }
+
+    const menuDom = document.querySelector(
+      ".bpx-player-ctrl-playbackrate-menu"
+    );
+
+    if (menuDom) {
+      menuDom.innerHTML = str;
+    }
   }
 
-  const menuDom = document.querySelector(".bpx-player-ctrl-playbackrate-menu");
+  let timer = setInterval(() => {
+    const menuDom = document.querySelector(
+      ".bpx-player-ctrl-playbackrate-menu"
+    );
 
-  if (menuDom) {
-    menuDom.innerHTML = str;
-  }
+    if (!menuDom) return;
+
+    exec();
+    clearInterval(timer);
+  }, 500);
 })();
